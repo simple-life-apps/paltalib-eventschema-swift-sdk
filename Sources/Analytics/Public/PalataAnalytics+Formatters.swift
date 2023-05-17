441ecbd7a5f52005ade6245990dd3bfd53df6cfc
Vyacheslav Beltyukov <vyacheslav@beltyukov.mobi>
2023-05-17 18:07:15 +0300

    Implement standard formatters

--- Sources/Analytics/Public/PalataAnalytics+Formatters.swift
+++ Sources/Analytics/Public/PalataAnalytics+Formatters.swift
@@ -11,8 +11,13 @@ public extension PaltaAnalytics {
     enum Formatters {
         public static let dateFormatter = DateFormatter().do {
             $0.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+            $0.timeZone = TimeZone(secondsFromGMT: 0)
         }
         
-        public static let decimalFormatter = NSDecimalNumber
+        public static let decimalFormatter = NumberFormatter().do {
+            $0.decimalSeparator = "."
+            $0.maximumSignificantDigits = 10
+            $0.maximumFractionDigits = 10
+        }
     }
 }
---
+++ Tests/Analytics/Tests/PublicFormattersTests.swift
@@ -0,0 +1,83 @@
+//
+//  PublicFormattersTests.swift
+//  
+//
+//  Created by Vyacheslav Beltyukov on 17/05/2023.
+//
+
+import PaltaAnalytics
+import XCTest
+
+final class PublicFormattersTests: XCTestCase {
+    func testDate() {
+        let dateComponents = DateComponents(
+            calendar: .current,
+            timeZone: TimeZone(secondsFromGMT: 0),
+            year: 1993,
+            month: 3,
+            day: 25,
+            hour: 21,
+            minute: 25,
+            second: 1,
+            nanosecond: 805000000
+        )
+        
+        let date = dateComponents.date!
+        
+        let string = PaltaAnalytics.Formatters.dateFormatter.string(from: date)
+        
+        XCTAssertEqual(
+            string,
+            "1993-03-25 21:25:01.805"
+        )
+    }
+    
+    func testDateZeroes() {
+        let dateComponents = DateComponents(
+            calendar: .current,
+            timeZone: TimeZone(secondsFromGMT: 0),
+            year: 2019,
+            month: 9,
+            day: 15
+        )
+        
+        let date = dateComponents.date!
+        
+        let string = PaltaAnalytics.Formatters.dateFormatter.string(from: date)
+        
+        XCTAssertEqual(
+            string,
+            "2019-09-15 00:00:00.000"
+        )
+    }
+    
+    func testRoundDecimal() {
+        let decimal: Decimal = 9856874665
+        let string = PaltaAnalytics.Formatters.decimalFormatter.string(from: decimal as NSDecimalNumber)
+        
+        XCTAssertEqual(
+            string,
+            "9856874665"
+        )
+    }
+    
+    func testDecimal() {
+        let decimal: Decimal = 9856874.665
+        let string = PaltaAnalytics.Formatters.decimalFormatter.string(from: decimal as NSDecimalNumber)
+        
+        XCTAssertEqual(
+            string,
+            "9856874.665"
+        )
+    }
+    
+    func testNonDouble() {
+        let decimal: Decimal = 1.017
+        let string = PaltaAnalytics.Formatters.decimalFormatter.string(from: decimal as NSDecimalNumber)
+        
+        XCTAssertEqual(
+            string,
+            "1.017"
+        )
+    }
+}
